b0ba96b584d6b856aab38734001a33417afdf960
Jeronimo Corigliano <82349162+Krilzs@users.noreply.github.com>
2025-08-26 00:19:48 -0300

    Update page.js

--- src/app/pending/page.js
+++ src/app/pending/page.js
@@ -1,27 +1,22 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-
-export default function PendingPage() {
-  const searchParams = useSearchParams();
-  const paymentId = searchParams.get("payment_id");
-  const status = searchParams.get("status");
-
+export default async function PendingPage({}) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-yellow-50 text-center p-6">
       <h1 className="text-3xl font-bold text-yellow-600 mb-4">
         ⏳ Pago en proceso
       </h1>
       <p className="text-lg text-gray-700">
-        Tu pago está pendiente de confirmación. Te notificaremos cuando se apruebe ✅
+        Tu pago está pendiente de confirmación. Te notificaremos cuando se
+        apruebe ✅
       </p>
 
       <div className="mt-6 bg-white shadow-md rounded-lg p-4">
-        <p><strong>ID de pago:</strong> {paymentId}</p>
-        <p><strong>Estado:</strong> {status}</p>
+        <p>
+          <strong>ID de pago:</strong>
+        </p>
+        <p>
+          <strong>Estado:</strong>
+        </p>
       </div>
     </div>
   );
 }
-
-
-export const dynamic = "force-dynamic";
