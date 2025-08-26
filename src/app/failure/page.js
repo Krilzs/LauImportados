ebcac56a46b9fe5be4f26b0bce2ff21bdbb36e4f
Jeronimo Corigliano <82349162+Krilzs@users.noreply.github.com>
2025-08-26 00:20:02 -0300

    Update page.js

--- src/app/failure/page.js
+++ src/app/failure/page.js
@@ -1,11 +1,5 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-
-export default function FailurePage() {
-  const searchParams = useSearchParams();
-  const paymentId = searchParams.get("payment_id");
-  const status = searchParams.get("status");
-
+// /app/failure/page.js
+export default async function FailurePage({}) {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-red-50 text-center p-6">
       <h1 className="text-3xl font-bold text-red-700 mb-4">
@@ -17,14 +11,12 @@ export default function FailurePage() {
 
       <div className="mt-6 bg-white shadow-md rounded-lg p-4">
         <p>
-          <strong>ID de pago:</strong> {paymentId}
+          <strong>ID de pago:</strong>
         </p>
         <p>
-          <strong>Estado:</strong> {status}
+          <strong>Estado:</strong>
         </p>
       </div>
     </div>
   );
 }
-
-export const dynamic = "force-dynamic";
